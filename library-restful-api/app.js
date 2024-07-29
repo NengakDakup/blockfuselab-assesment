cfcb1c3751886fa6d5aab3faa62014e203be5041
Nengak Dakup <nengakdakup@gmail.com>
2024-07-29 11:37:29 +0100

    Added app file

---
+++ library-restful-api/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+require('dotenv').config();
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database'));
+
+app.use(express.json());
+
+module.exports = app;
